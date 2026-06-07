02fd70d95c789fd2a3a76cf0c0c3fa5460e749ac
Eli Foley <elliefol@hotmail.com>
2026-06-07 13:04:48 -0400

    added opening hours to about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -9,7 +9,9 @@ function AboutUs() {
           <h2>About Us</h2>
           <p>Established in 2013, we are a locally owned and operated family business. Our kind and understanding customer service makes us stand out from the rest. When you come into our shop, you can be assured that our staff will do everything they can to ensure you're ready for your next biking adventure - whether you're a seasoned cyclist or just starting out. <br />
             Our location is fully accessible by public transit, and there is lots of street parking available nearby. <br />
-            Ready to get out there? Give us a call for any questions, or come see us in-store and you'll be rolling in no time!</p>
+            Ready to get out there? Give us a call for any questions, or come see us in-store and you'll be rolling in no time!<br/>
+            <br />
+            Open 9 AM to 6 PM every day!</p>
         </div>
     </div>;
 }
